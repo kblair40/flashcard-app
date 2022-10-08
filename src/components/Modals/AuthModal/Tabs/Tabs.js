6f8a9d4d62206c2b93cfa7af52ace81021061769
Kevin Blair <kblair40@gmail.com>
2022-10-08 07:34:40 -0500

    Add skeletons for SigninForm, SignupForm and Tabs

---
+++ src/components/Modals/AuthModal/Tabs/Tabs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+
+import SignupForm from "../SignupForm";
+import SigninForm from "../SigninForm";
+
+const AuthModalTabs = () => {
+  return <div>Tabs</div>;
+};
+
+export default AuthModalTabs;
