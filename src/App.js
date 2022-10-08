e824dfaa5526c30e3e7a493915de5f5a8b954f0c
Kevin Blair <kblair40@gmail.com>
2022-10-08 10:22:29 -0500

    Setup UserContext provider in App

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 
+import { UserProvider } from "store/UserContext";
 import Home from "pages/Home";
 import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Box minH="100vh">
-      <Navbar />
+    <UserProvider>
+      <Box minH="100vh">
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Box>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Box>
+    </UserProvider>
   );
 }
 
