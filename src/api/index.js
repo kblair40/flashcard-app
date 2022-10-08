00b13829103e3e7917b17e6f2da5bb39ed3c4309
Kevin Blair <kblair40@gmail.com>
2022-10-08 17:17:40 -0500

    Add interceptor to add auth_token to api requests

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,21 @@ const api = axios.create({
   baseURL: "http://localhost:4000/",
 });
 
+api.interceptors.request.use(
+  // called everytime we make a request
+  async (config) => {
+    const my_token = await window.localStorage.getItem("auth-token");
+
+    if (my_token) {
+      config.headers.Authorization = `Bearer ${my_token}`;
+    }
+    return config;
+  },
+  // called only when there is an error in the request
+  (err) => {
+    console.log("ERROR IN AXIOS INSTANCE");
+    return Promise.reject(err);
+  }
+);
+
 export default api;
