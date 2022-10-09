0f6130c7e29c38daea0702b8f2f8e37e4756284f
Kevin Blair <kblair40@gmail.com>
2022-10-09 09:25:33 -0500

    Move set fetch function to SetContext

--- src/components/CreateSet/CreateSet.js
+++ src/components/CreateSet/CreateSet.js
@@ -1,34 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext } from "react";
 import { Flex } from "@chakra-ui/react";
 
+import SetContext from "store/SetContext";
 import Sidebar from "./Sidebar";
 import Editors from "./Editors";
-import api from "api";
 
 const CreateSet = () => {
-  const [flashcardSetData, setFlashcardSetData] = useState();
-
-  const params = useParams();
-
-  useEffect(() => {
-    const fetchSet = async (id) => {
-      try {
-        const response = await api.get(`/flashcard_set/${id}`);
-        console.log("\nRESPONSE:", response.data, "\n");
-
-        if (response.data && response.data.set) {
-          setFlashcardSetData(response.data.set);
-        }
-      } catch (e) {
-        console.log("FAILED FETCHING SET:", e);
-      }
-    };
-
-    if (params.id) {
-      fetchSet(params.id);
-    }
-  }, [params]);
+  const { flashcardSetData } = useContext(SetContext);
 
   return (
     <Flex justify="center" w="100%" h="calc(100vh - 60px)" overflowY="hidden">
--- src/store/SetContext.js
+++ src/store/SetContext.js
@@ -1,10 +1,35 @@
 import { createContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "api";
 
 const SetContext = createContext();
 
 const SetProvider = ({ children }) => {
+  const [flashcardSetData, setFlashcardSetData] = useState();
   const [frontCardContent, setFrontCardContent] = useState("");
   const [backCardContent, setBackCardContent] = useState("");
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchSet = async (id) => {
+      try {
+        const response = await api.get(`/flashcard_set/${id}`);
+        console.log("\nRESPONSE:", response.data, "\n");
+
+        if (response.data && response.data.set) {
+          setFlashcardSetData(response.data.set);
+        }
+      } catch (e) {
+        console.log("FAILED FETCHING SET:", e);
+      }
+    };
+
+    if (params.id) {
+      fetchSet(params.id);
+    }
+  }, [params]);
 
   return (
     <SetContext.Provider
@@ -13,6 +38,7 @@ const SetProvider = ({ children }) => {
         setFrontCardContent,
         backCardContent,
         setBackCardContent,
+        flashcardSetData,
       }}
     >
       {children}
