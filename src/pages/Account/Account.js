98c53ee0eb04a81119c49960a840fba8d73d5e0c
Kevin Blair <kblair40@gmail.com>
2022-10-09 18:12:03 -0500

    add skeleton for Account page

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Box, Flex, Text, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Account = () => {
-  return <div>Account</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Flex justify="center">
+      <Flex
+        direction="column"
+        w="100%"
+        pt="2rem"
+        maxW={{ base: "300px", sm: "400px" }}
+      >
+        <Button onClick={() => navigate(`/accounts/sets`)}>Manage Sets</Button>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Account;
