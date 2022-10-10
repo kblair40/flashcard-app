7b8f3fc01372bdc844a1fa27752ce7cd34a86bda
Kevin Blair <kblair40@gmail.com>
2022-10-10 18:31:43 -0500

    Finish entrance animation

--- src/pages/StudySession/CurrentCard/CurrentCard.js
+++ src/pages/StudySession/CurrentCard/CurrentCard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactCardFlip from "react-card-flip";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { Box, Flex, Button } from "@chakra-ui/react";
 
@@ -43,23 +44,30 @@ const FlipContainer = ({ children }) => {
   if (!children) return null;
 
   return (
-    <>
-      <ReactCardFlip
-        flipSpeedBackToFront={0.3}
-        flipSpeedFrontToBack={0.3}
-        isFlipped={side === "back"}
-        flipDirection="vertical"
+    <AnimatePresence>
+      <motion.div
+        initial={{ x: "-100%" }}
+        animate={{ x: 0 }}
+        exit={{ opacity: 0.4 }}
+        transition={{ duration: 1 }}
       >
-        {children}
-      </ReactCardFlip>
-      <Button
-        mt=".75rem"
-        variant="ghost"
-        onClick={() => setSide(side === "back" ? "front" : "back")}
-      >
-        Flip
-      </Button>
-    </>
+        <ReactCardFlip
+          flipSpeedBackToFront={0.3}
+          flipSpeedFrontToBack={0.3}
+          isFlipped={side === "back"}
+          flipDirection="vertical"
+        >
+          {children}
+        </ReactCardFlip>
+        <Button
+          mt=".75rem"
+          variant="ghost"
+          onClick={() => setSide(side === "back" ? "front" : "back")}
+        >
+          Flip
+        </Button>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
@@ -82,3 +90,90 @@ const Flashcard = ({ content }) => {
     </Flex>
   );
 };
+
+//
+// BACKUP COPY
+// import React, { useState, useEffect, useRef } from "react";
+// import ReactCardFlip from "react-card-flip";
+
+// import { Box, Flex, Button } from "@chakra-ui/react";
+
+// const MotionContainer = ({ flashcards, currentCard }) => {
+//   const [flipCards, setFlipCards] = useState();
+
+//   const didMount = useRef(false);
+//   useEffect(() => {
+//     if (didMount.current || !flashcards) return;
+
+//     didMount.current = true;
+
+//     let flipCardsArray = [];
+//     flashcards.forEach((card, i) => {
+//       const { front_content, back_content } = card;
+
+//       flipCardsArray.push(
+//         <FlipContainer key={i}>
+//           <Flashcard content={front_content} side="front" />
+//           <Flashcard content={back_content} side="back" />
+//         </FlipContainer>
+//       );
+//     });
+
+//     setFlipCards(flipCardsArray);
+//   }, [flashcards]);
+
+//   if (!flipCards || !flipCards.length) return null;
+
+//   return (
+//     <Flex direction="column" w="100%" align="center">
+//       {flipCards[currentCard]}
+//     </Flex>
+//   );
+// };
+
+// export default MotionContainer;
+
+// const FlipContainer = ({ children }) => {
+//   const [side, setSide] = useState("front");
+//   if (!children) return null;
+
+//   return (
+//     <>
+//       <ReactCardFlip
+//         flipSpeedBackToFront={0.3}
+//         flipSpeedFrontToBack={0.3}
+//         isFlipped={side === "back"}
+//         flipDirection="vertical"
+//       >
+//         {children}
+//       </ReactCardFlip>
+//       <Button
+//         mt=".75rem"
+//         variant="ghost"
+//         onClick={() => setSide(side === "back" ? "front" : "back")}
+//       >
+//         Flip
+//       </Button>
+//     </>
+//   );
+// };
+
+// const Flashcard = ({ content }) => {
+//   return (
+//     <Flex
+//       justify="center"
+//       align="center"
+//       h={{ base: "250px", sm: "260px", md: "340px" }}
+//       w={{ base: "340px", sm: "440px", md: "550px" }}
+//       borderRadius="2px"
+//       shadow="sm"
+//       bg="gray.50"
+//     >
+//       <Box
+//         dangerouslySetInnerHTML={{
+//           __html: content ? content : "<div />",
+//         }}
+//       />
+//     </Flex>
+//   );
+// };
--- src/pages/StudySession/StudySession.js
+++ src/pages/StudySession/StudySession.js
@@ -101,3 +101,110 @@ const StudySession = () => {
 };
 
 export default StudySession;
+
+//
+//
+// BACKUP COPY
+// import React, { useEffect, useState } from "react";
+// import { Button, Flex, Heading, Center, Spinner } from "@chakra-ui/react";
+// import { useParams } from "react-router-dom";
+
+// import CurrentCard from "./CurrentCard";
+// import AllCards from "./AllCards";
+// import api from "api";
+
+// const StudySession = () => {
+//   const [currentCard, setCurrentCard] = useState(0);
+//   const [loading, setLoading] = useState(true);
+//   const [flashcards, setFlashcards] = useState();
+//   const [title, setTitle] = useState("");
+//   const [hideAllCards, setHideAllCards] = useState(false);
+
+//   const params = useParams();
+
+//   useEffect(() => {
+//     const fetchSet = async (setId) => {
+//       try {
+//         const response = await api.get(`/flashcard_set/${setId}`);
+//         console.log("\nSET RESPONSE:", response.data);
+
+//         if (response.data && response.data.set) {
+//           const { flashcards, title } = response.data.set;
+//           setFlashcards(flashcards);
+//           setTitle(title);
+//         }
+//       } catch (e) {
+//         console.error("FAILED FETCHING SET:", e);
+//       }
+//       setLoading(false);
+//     };
+
+//     if (params && params.id) {
+//       fetchSet(params.id);
+//     }
+//   }, [params]);
+
+//   const handleClickCardPreview = (index) => {
+//     console.log("INDEX:", index);
+//     setCurrentCard(index);
+//   };
+
+//   if (loading) {
+//     return (
+//       <Center h="calc(100vh - 60px)" overflowY="hidden">
+//         <Spinner />
+//       </Center>
+//     );
+//   }
+
+//   return (
+//     <Flex
+//       justify="center"
+//       pt="2rem"
+//       h="calc(100vh - 60px)"
+//       position="relative"
+//       overflowY="hidden"
+//     >
+//       <Flex w="100%" maxW={{ base: "90%" }} direction="column" align="center">
+//         <Heading mb="1.5rem" textAlign="center">
+//           {title}
+//         </Heading>
+
+//         <CurrentCard flashcards={flashcards} currentCard={currentCard} />
+
+//         <Flex justify="center" w="100%" mt="1.5rem">
+//           <Button
+//             mr="1rem"
+//             isDisabled={currentCard === 0}
+//             onClick={() => setCurrentCard((prev) => prev - 1)}
+//           >
+//             Prev Card
+//           </Button>
+//           <Button
+//             onClick={() => setCurrentCard((prev) => prev + 1)}
+//             isDisabled={flashcards && flashcards.length - 1 === currentCard}
+//           >
+//             Next Card
+//           </Button>
+//         </Flex>
+//       </Flex>
+
+//       <AllCards
+//         show={!hideAllCards}
+//         onClickCard={handleClickCardPreview}
+//         flashcards={flashcards}
+//       />
+//       <Button
+//         size="sm"
+//         onClick={() => setHideAllCards(!hideAllCards)}
+//         position="fixed"
+//         bottom="10rem"
+//         left="1rem"
+//       >
+//         Hide/Show
+//       </Button>
+//     </Flex>
+//   );
+// };
+
+// export default StudySession;
