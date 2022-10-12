f0aad317eb8c6268bde592cb7f6144cc983c3c34
Kevin Blair <kblair40@gmail.com>
2022-10-12 06:01:32 -0500

    Add primary color and secondary textStyle to theme

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import "./index.css";
+import theme from "utils/theme";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </BrowserRouter>
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,5 +1,30 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  colors: {},
+  colors: {
+    primary: {
+      base: "#1A5CFB",
+      50: "#e1edff",
+      100: "#b1c9ff",
+      200: "#80a6ff",
+      300: "#4e82fd",
+      400: "#1d5efb",
+      500: "#0445e2",
+      600: "#0036b0",
+      700: "#00267f",
+      800: "#00174f",
+      900: "#000820",
+    },
+  },
+  // textStyles: {
+  //   secondary: { color: "gray.600" },
+  // },
+  textStyles: (...args) => {
+    console.log("ARGS:", args);
+    return {
+      secondary: { color: "gray.600" },
+    };
+  },
 });
+
+export default theme;
