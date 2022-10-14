e09e700a3e09b5956c02e7582b0a58d66e9091ed
Kevin Blair <kblair40@gmail.com>
2022-10-14 14:24:07 -0500

    add getCleanDuration helper function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12,3 +12,23 @@ export const makeDisplayName = (userData) => {
 
   return `${first_name} ${last_name}`;
 };
+
+export const getCleanDuration = (duration) => {
+  const { hours, minutes, seconds } = duration;
+
+  let res;
+  if (hours) {
+    res = `${hours} hours`;
+    if (minutes) res += `, ${minutes} minutes`;
+    if (seconds) res += `, ${seconds} seconds`;
+  } else if (minutes) {
+    res = `${minutes} minutes`;
+    if (seconds) res += `, ${seconds} seconds`;
+  } else if (seconds) {
+    res = `${seconds} seconds`;
+  } else {
+    return null;
+  }
+
+  return res;
+};
