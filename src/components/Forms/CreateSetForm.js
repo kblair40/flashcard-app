bf53f1bb89e1f96d6ea426a90fbb3c2c00d03eae
Kevin Blair <kblair40@gmail.com>
2022-10-15 10:15:42 -0500

    Add public status to required form data

--- src/components/Forms/CreateSetForm.js
+++ src/components/Forms/CreateSetForm.js
@@ -6,6 +6,7 @@ import {
   VStack,
   Flex,
   Button,
+  Checkbox,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
@@ -19,13 +20,14 @@ const CreateSetForm = () => {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
+    is_public: true,
   });
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState(INITIAL_ERROR_STATE);
 
   const handleSubmit = async () => {
     setLoading(true);
-    const { title, description } = formData;
+    const { title, description, is_public } = formData;
 
     if (!title) {
       setErrors({ ...errors, title: true });
@@ -37,6 +39,7 @@ const CreateSetForm = () => {
       const response = await api.post("/flashcard_set", {
         title,
         description,
+        public: is_public,
       });
       console.log("\nCREATE RESPONSE:", response.data);
 
@@ -54,6 +57,7 @@ const CreateSetForm = () => {
   return (
     <Flex w="100%" justify="center" pt="2rem">
       <VStack
+        align="start"
         w="100%"
         spacing="1.5rem"
         maxW={{ base: "90%", sm: "450px", md: "540px" }}
@@ -76,6 +80,17 @@ const CreateSetForm = () => {
             }
           />
         </FormControl>
+
+        <Checkbox
+          spacing=".75rem"
+          isChecked={formData.is_public}
+          onChange={(e) =>
+            setFormData({ ...formData, is_public: e.target.checked })
+          }
+          fontWeight={500}
+        >
+          Make Set Public
+        </Checkbox>
 
         <Button w="100%" onClick={handleSubmit} isLoading={loading}>
           Create
