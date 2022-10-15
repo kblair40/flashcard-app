afc2e6dfcdd5a7baa1ba5432ace12e6163965f39
Kevin Blair <kblair40@gmail.com>
2022-10-15 06:52:32 -0500

    Add autoclearing to input on path change and change handler function

--- src/components/Navbar/SetSearch.js
+++ src/components/Navbar/SetSearch.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
 
 const SetSearch = () => {
-  return <Input minW="200px" borderColor="gray.300" placeholder="Search" />;
+  const [value, setValue] = useState("");
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Clear input if user changes path
+    setValue("");
+  }, [pathname]);
+
+  const handleChange = async (e) => {
+    const { value } = e.target;
+    setValue(value);
+  };
+
+  return (
+    <Input
+      value={value}
+      onChange={handleChange}
+      minW="200px"
+      borderColor="gray.300"
+      placeholder="Search"
+    />
+  );
 };
 
 export default SetSearch;
