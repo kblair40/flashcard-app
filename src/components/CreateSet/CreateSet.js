113d1052a2babd43b366f52bb59a93c012486367
Kevin Blair <kblair40@gmail.com>
2022-10-18 07:09:57 -0500

    Add functional skeleton for SetMeta component

--- src/components/CreateSet/CreateSet.js
+++ src/components/CreateSet/CreateSet.js
@@ -1,21 +1,50 @@
-import React from // { useContext }
-"react";
-import { Flex } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { Flex, Text, Box } from "@chakra-ui/react";
 
-// import SetContext from "store/SetContext";
+import SetContext from "store/SetContext";
 import Sidebar from "./Sidebar";
 import Editors from "./Editors";
 
 const CreateSet = () => {
-  // const setCtx = useContext(SetContext);
+  const { flashcardSetData: setData } = useContext(SetContext);
+  // console.log("SET CTX:", setData);
 
   return (
     <Flex justify="center" w="100%" h="calc(100vh - 60px)" overflowY="hidden">
       <Sidebar width={{ base: "100%", sm: "30%", md: "25%" }} />
 
-      <Editors width={{ base: "100%", sm: "70%", md: "75%" }} />
+      <Flex
+        direction="column"
+        h="100%"
+        width={{ base: "100%", sm: "70%", md: "75%" }}
+      >
+        <SetMeta setData={setData} />
+        <Editors width={{ base: "100%" }} />
+      </Flex>
     </Flex>
   );
 };
 
 export default CreateSet;
+
+const SetMeta = ({ setData, height = "100%", width = "100%" }) => {
+  let category, title;
+
+  if (setData) {
+    category = setData.category;
+    title = setData.title;
+  }
+  return (
+    <Flex height={height} width={width}>
+      <Box>
+        <Text fontWeight={600}>Category:</Text>
+        <Text>{category}</Text>
+      </Box>
+
+      <Box ml="1rem">
+        <Text fontWeight={600}>Title:</Text>
+        <Text>{title}</Text>
+      </Box>
+    </Flex>
+  );
+};
