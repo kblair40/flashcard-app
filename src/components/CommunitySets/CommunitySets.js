75a34e50d9bc3cd211ebcf275c5c0874cf241401
Kevin Blair <kblair40@gmail.com>
2022-10-19 07:19:12 -0500

    Add fetch function to retrieve community sets and add loading indicator

--- src/components/CommunitySets/CommunitySets.js
+++ src/components/CommunitySets/CommunitySets.js
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { Box, Text, Flex, Heading } from "@chakra-ui/react";
+import { Box, Text, Flex, Heading, Spinner } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 import api from "api";
 
 const headingStyles = {
   textTransform: "capitalize",
   fontWeight: "700",
-  // pl: "16px",
   fontSize: { base: "xl", sm: "2xl" },
 };
 
 const CommunitySets = () => {
+  const [loading, setLoading] = useState(true);
+  const [communitySets, setCommunitySets] = useState();
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const response = await api.get("/community_sets");
+
+        if (response.data) {
+          console.log("COMMUNITY RESPONSE:", response.data);
+          setCommunitySets(response.data);
+        }
+      } catch (e) {
+        console.error("FAILED FETCHING HISTORY:", e);
+      }
+      setLoading(false);
+    };
+
+    fetchHistory();
+  }, []);
+
   return (
     <Flex justify="center" pt="32px" w="100%">
       <Flex
@@ -29,6 +50,16 @@ const CommunitySets = () => {
           <Heading {...headingStyles} noOfLines={2}>
             Latest Sets from the Community
           </Heading>
+
+          {loading ? (
+            <Flex h="200px" w="100%" justify="center" align="center">
+              <Spinner />
+            </Flex>
+          ) : communitySets && communitySets.length ? (
+            communitySets.map((set, idx) => {
+              return <div />;
+            })
+          ) : null}
         </Flex>
       </Flex>
     </Flex>
