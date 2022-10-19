07605b3f17af8ab5f1d73965075eaa5de8cda29e
Kevin Blair <kblair40@gmail.com>
2022-10-19 07:26:43 -0500

    Finish CommunitySet styles

--- src/components/CommunitySets/CommunitySets.js
+++ src/components/CommunitySets/CommunitySets.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Box, Text, Flex, Heading, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Flex,
+  Heading,
+  Spinner,
+  useColorMode,
+  IconButton,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
+import { TrashIcon } from "utils/icons";
 import api from "api";
 
 const headingStyles = {
@@ -50,20 +59,65 @@ const CommunitySets = () => {
           <Heading {...headingStyles} noOfLines={2}>
             Latest Sets from the Community
           </Heading>
-
-          {loading ? (
-            <Flex h="200px" w="100%" justify="center" align="center">
-              <Spinner />
-            </Flex>
-          ) : communitySets && communitySets.length ? (
-            communitySets.map((set, idx) => {
-              return <div />;
-            })
-          ) : null}
         </Flex>
+
+        {loading ? (
+          <Flex h="200px" w="100%" justify="center" align="center">
+            <Spinner />
+          </Flex>
+        ) : communitySets && communitySets.length ? (
+          communitySets.reverse().map((set, idx) => {
+            return (
+              <CommunitySet
+                key={idx}
+                set={set}
+                onClick={() => console.log("clicked")}
+              />
+            );
+          })
+        ) : null}
       </Flex>
     </Flex>
   );
 };
 
 export default CommunitySets;
+
+const CommunitySet = ({ set, onClick }) => {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  if (!set) {
+    return null;
+  }
+
+  return (
+    <Flex
+      justify="space-between"
+      align="center"
+      py="8px"
+      borderRadius="4px"
+      w="100%"
+    >
+      <Flex direction="column" w="100%">
+        <Text mr="6px" mb="6px" fontSize="lg" fontWeight="600" lineHeight={1}>
+          {set.title}
+        </Text>
+
+        <Text lineHeight={1} fontSize="sm">
+          Created {new Date(set.createdAt).toLocaleDateString()}
+        </Text>
+      </Flex>
+
+      <IconButton
+        onClick={onClick}
+        size="sm"
+        bg={isDark ? "gray.800" : "#fff"}
+        variant="ghost"
+        rounded="full"
+        aria-label="Delete Button"
+        icon={<TrashIcon boxSize="18px" />}
+      />
+    </Flex>
+  );
+};
