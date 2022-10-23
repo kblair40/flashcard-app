9513d9b94ecf3dc94547985b20ded814a1fc4667
Kevin Blair <kblair40@gmail.com>
2022-10-23 08:12:41 -0500

    Remove font size options

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const quillSettings = {
   modules: {
     toolbar: [
-      [{ size: ["small", "medium", "large"] }],
+      // [{ size: ["small", "medium", "large"] }],
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
       ["bold", "italic", "underline"],
       [
