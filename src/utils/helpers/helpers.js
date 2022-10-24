80d1e6f9499c1730a70dfdf90b07aa39600401cb
Kevin Blair <kblair40@gmail.com>
2022-10-24 07:31:25 -0500

    Add toTitleCase helper function

--- src/utils/helpers/helpers.js
+++ src/utils/helpers/helpers.js
@@ -34,3 +34,13 @@ export const getCleanDuration = (duration) => {
 
   return res;
 };
+
+export const toTitleCase = (str) => {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(function (word) {
+      return word.replace(word[0], word[0].toUpperCase());
+    })
+    .join(" ");
+};
