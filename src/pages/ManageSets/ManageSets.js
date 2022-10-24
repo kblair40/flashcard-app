2573ca3d029e86fe7126e22aefd1a558ef3982be
Kevin Blair <kblair40@gmail.com>
2022-10-24 15:29:38 -0500

    Improve empty state styles

--- src/pages/ManageSets/ManageSets.js
+++ src/pages/ManageSets/ManageSets.js
@@ -172,133 +172,136 @@ const CreatedSets = ({
         </Heading>
       </Box>
 
-      <Grid
-        p={{ base: "0 .5rem .5rem", sm: "0 1rem .5rem" }}
-        maxH={{ base: "45vh", md: "calc(100vh - 186px)" }}
-        overflowY="auto"
-        templateRows="auto"
-        w="100%"
-        templateColumns={{
-          base: "3fr 2.5fr 1fr 2fr",
-          sm: "2fr 1.5fr 1fr 1fr",
-          lg: "2fr 1fr 1.5fr 1fr 1fr",
-        }}
-        columnGap={{ base: "4", sm: "8" }}
-        rowGap="3"
-        alignItems="center"
-      >
-        {["Title", "# of Cards", "Last Updated", "Public", ""].map(
-          (header, i) => {
-            return (
-              <GridItem
-                justifySelf={[1, 2, 3].includes(i) ? "center" : undefined}
-                // border=".1px solid #fff"
-                key={i}
-                display={i === 2 ? { base: "none", lg: "block" } : "block"}
-              >
-                <Text fontWeight="600" fontSize={{ base: "sm" }}>
-                  {header}
-                </Text>
-              </GridItem>
-            );
-          }
-        )}
-
-        {flashcardSets && flashcardSets.length
-          ? flashcardSets.map((set, i) => {
-              const {
-                updatedAt,
-                title,
-                flashcards,
-                _id,
-                public: isPublic,
-              } = set;
-
-              const lastUpdated = new Date(updatedAt).toLocaleDateString();
-
+      {flashcardSets && flashcardSets.length ? (
+        <Grid
+          border="1px solid white"
+          p={{ base: "0 .5rem .5rem", sm: "0 1rem .5rem" }}
+          maxH={{ base: "45vh", md: "calc(100vh - 186px)" }}
+          overflowY="auto"
+          templateRows="auto"
+          w="100%"
+          templateColumns={{
+            base: "3fr 2.5fr 1fr 2fr",
+            sm: "2fr 1.5fr 1fr 1fr",
+            lg: "2fr 1fr 1.5fr 1fr 1fr",
+          }}
+          columnGap={{ base: "4", sm: "8" }}
+          rowGap="3"
+          alignItems="center"
+        >
+          {["Title", "# of Cards", "Last Updated", "Public", ""].map(
+            (header, i) => {
               return (
-                <React.Fragment key={i}>
-                  <GridItem w="100%" maxW={{ base: "100px", sm: "140px" }}>
-                    <Text noOfLines={1} fontWeight="600" w="100%">
-                      {title}
-                    </Text>
-                  </GridItem>
-
-                  <GridItem display="flex" justifyContent="center">
-                    <Text fontWeight="600">{flashcards.length}</Text>
-                  </GridItem>
-
-                  <GridItem
-                    justifyContent="center"
-                    display={{ base: "none", lg: "flex" }}
-                  >
-                    <Text fontWeight="600">{lastUpdated}</Text>
-                  </GridItem>
-
-                  <GridItem display="flex" justifyContent="center">
-                    {changingPublicStatus === _id ? (
-                      <Center>
-                        <Spinner />
-                      </Center>
-                    ) : (
-                      <Checkbox
-                        colorScheme="gray"
-                        isDisabled={changingPublicStatus === _id}
-                        isChecked={isPublic}
-                        onChange={(e) => handleChangePublicStatus(e, _id)}
-                      />
-                    )}
-                  </GridItem>
-                  {/* 5hr 53min */}
-                  <GridItem>
-                    <Link to={`/create/${_id}`}>
-                      <Button
-                        bg="transparent"
-                        transition="background-color 0.2s"
-                        _hover={{ bg: isDark ? "gray.700" : "gray.100" }}
-                        _active={{ bg: isDark ? "gray.600" : "gray.200" }}
-                        leftIcon={<EditIcon boxSize="14px" />}
-                        size="sm"
-                        w="100%"
-                      >
-                        Edit
-                      </Button>
-                    </Link>
-                  </GridItem>
-                </React.Fragment>
+                <GridItem
+                  justifySelf={[1, 2, 3].includes(i) ? "center" : undefined}
+                  // border=".1px solid #fff"
+                  key={i}
+                  display={i === 2 ? { base: "none", lg: "block" } : "block"}
+                >
+                  <Text fontWeight="600" fontSize={{ base: "sm" }}>
+                    {header}
+                  </Text>
+                </GridItem>
               );
-            })
-          : null}
+            }
+          )}
 
-        {flashcardSets && !flashcardSets.length ? (
-          <NoSets isDark={isDark} />
-        ) : null}
-      </Grid>
+          {flashcardSets && flashcardSets.length
+            ? flashcardSets.map((set, i) => {
+                const {
+                  updatedAt,
+                  title,
+                  flashcards,
+                  _id,
+                  public: isPublic,
+                } = set;
+
+                const lastUpdated = new Date(updatedAt).toLocaleDateString();
+
+                return (
+                  <React.Fragment key={i}>
+                    <GridItem w="100%" maxW={{ base: "100px", sm: "140px" }}>
+                      <Text noOfLines={1} fontWeight="600" w="100%">
+                        {title}
+                      </Text>
+                    </GridItem>
+
+                    <GridItem display="flex" justifyContent="center">
+                      <Text fontWeight="600">{flashcards.length}</Text>
+                    </GridItem>
+
+                    <GridItem
+                      justifyContent="center"
+                      display={{ base: "none", lg: "flex" }}
+                    >
+                      <Text fontWeight="600">{lastUpdated}</Text>
+                    </GridItem>
+
+                    <GridItem display="flex" justifyContent="center">
+                      {changingPublicStatus === _id ? (
+                        <Center>
+                          <Spinner />
+                        </Center>
+                      ) : (
+                        <Checkbox
+                          colorScheme="gray"
+                          isDisabled={changingPublicStatus === _id}
+                          isChecked={isPublic}
+                          onChange={(e) => handleChangePublicStatus(e, _id)}
+                        />
+                      )}
+                    </GridItem>
+                    {/* 5hr 53min */}
+                    <GridItem>
+                      <Link to={`/create/${_id}`}>
+                        <Button
+                          bg="transparent"
+                          transition="background-color 0.2s"
+                          _hover={{ bg: isDark ? "gray.700" : "gray.100" }}
+                          _active={{ bg: isDark ? "gray.600" : "gray.200" }}
+                          leftIcon={<EditIcon boxSize="14px" />}
+                          size="sm"
+                          w="100%"
+                        >
+                          Edit
+                        </Button>
+                      </Link>
+                    </GridItem>
+                  </React.Fragment>
+                );
+              })
+            : null}
+
+          {flashcardSets && !flashcardSets.length ? (
+            <NoSets isDark={isDark} />
+          ) : null}
+        </Grid>
+      ) : (
+        <NoSets isDark={isDark} />
+      )}
     </Box>
   );
 };
 
 const NoSets = ({ isDark }) => {
   return (
-    <Flex direction="column" h="200px" w="100%" justify="center" align="center">
+    <Flex direction="column" h="140px" w="100%" justify="center" align="center">
       <Text
         textAlign="center"
+        fontSize="lg"
         fontWeight="700"
         color={isDark ? "gray.50" : "gray.700"}
       >
-        NO SETS FOUND
+        You haven't created a set yet
       </Text>
 
       <Link to="/create">
         <Button
-          mt="2rem"
+          variant="solid-blue"
+          mt="1.5rem"
           rounded="full"
           size="lg"
           leftIcon={<AddIcon boxSize="16px" />}
-          _hover={{ bg: "primary.500" }}
-          _active={{ bg: "primary.500" }}
-          bg="primary.400"
-          color="white"
         >
           Create a Set
         </Button>
