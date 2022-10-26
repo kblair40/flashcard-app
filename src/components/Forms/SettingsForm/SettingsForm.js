a9a1fdb1885764ce8aac3d02d7f69b439955be42
Kevin Blair <kblair40@gmail.com>
2022-10-26 16:35:35 -0500

    Finish form input changes

--- src/components/Forms/SettingsForm/SettingsForm.js
+++ src/components/Forms/SettingsForm/SettingsForm.js
@@ -8,16 +8,19 @@ import {
   FormControl,
   FormLabel,
   Select,
-  CheckboxGroup,
   Checkbox,
   useColorMode,
+  Button,
 } from "@chakra-ui/react";
 
+import { toTitleCase } from "utils/helpers";
+import { options } from "./settings-options";
 import UserContext from "store/UserContext";
 
-const SettingsForm = ({ cardSide = "front" }) => {
+const SettingsForm = ({ cardSide }) => {
   const [formData, setFormData] = useState({
     textAlign: "left",
+    fontSize: "medium",
     isBold: false,
     isItalic: false,
     isUnderlined: false,
@@ -27,36 +30,114 @@ const SettingsForm = ({ cardSide = "front" }) => {
   const isDark = colorMode === "dark";
 
   const { userData } = useContext(UserContext);
+
+  const handleChangeCheck = (e) => {
+    const { value, checked } = e.target;
+    console.log("VALUE/CHECKED:", { value, checked });
+
+    setFormData({ ...formData, [`${value}`]: checked });
+  };
+
+  const handleChangeSelect = (e) => {
+    const { id, value } = e.target;
+
+    console.log("VALUE/ID:", { value, id });
+
+    setFormData({ ...formData, [`${id}`]: value });
+  };
+
   // CHECKBOX GROUP FOR BOLD, ITALIC AND UNDERLINE SETTINGS
   // SELECT FOR DEFAULT FONT SIZE
   // SELECT FOR ALIGNMENT
+  const checkboxProps = {
+    colorScheme: "gray",
+    onChange: (e) => handleChangeCheck(e),
+  };
+
   return (
     <Flex direction="column" align="center">
       <Stack
         p={{ base: "1.5rem 1rem", sm: "1.5rem" }}
         bg={isDark ? "gray.800" : "#fff"}
         borderRadius="10px"
-        // borderRadius={{ base: "10px", sm: "12px" }}
         boxShadow="md"
         align="center"
         justify="center"
         w="100%"
         spacing="1.5rem"
-        maxW={{ base: "90%", sm: "450px", md: "540px" }}
       >
-        <CheckboxGroup colorScheme="green">
+        <Flex direction="column">
+          <Text fontWeight="500" mb="6px" fontSize="md">
+            Text Styles
+          </Text>
           <Stack spacing={[1, 5]} direction={["column", "row"]}>
-            <Checkbox value="bold" fontWeight="700">
+            <Checkbox
+              {...checkboxProps}
+              isChecked={formData.isBold}
+              value="isBold"
+              fontWeight="700"
+            >
               Bold
             </Checkbox>
-            <Checkbox value="italic" fontStyle="italic">
+            <Checkbox
+              {...checkboxProps}
+              isChecked={formData.isItalic}
+              value="isItalic"
+              fontStyle="italic"
+            >
               Italic
             </Checkbox>
-            <Checkbox value="underlined" textDecoration="underline">
+            <Checkbox
+              {...checkboxProps}
+              isChecked={formData.isUnderlined}
+              value="isUnderlined"
+              textDecoration="underline"
+            >
               Underlined
             </Checkbox>
           </Stack>
-        </CheckboxGroup>
+        </Flex>
+
+        <FormControl>
+          <FormLabel fontSize="md" mb="6px">
+            Text Size
+          </FormLabel>
+          <Select
+            variant="neutral-outline"
+            onChange={handleChangeSelect}
+            value={formData.fontSize}
+            id="fontSize"
+          >
+            {options.sizes.map((opt, i) => {
+              return (
+                <option key={i} value={opt}>
+                  {toTitleCase(opt)}
+                </option>
+              );
+            })}
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel fontSize="md" mb="6px">
+            Text Alignment
+          </FormLabel>
+          <Select
+            variant="neutral-outline"
+            onChange={handleChangeSelect}
+            value={formData.textAlign}
+            id="textAlign"
+          >
+            {options.alignment.map((opt, i) => {
+              return (
+                <option key={i} value={opt}>
+                  {toTitleCase(opt)}
+                </option>
+              );
+            })}
+          </Select>
+        </FormControl>
+
         {/* <FormControl isRequired>
           <FormLabel>Set Title</FormLabel>
           <Input
