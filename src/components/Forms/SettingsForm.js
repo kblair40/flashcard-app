c28c72dee47a32cd45a931c976a46f7fcdcf4557
Kevin Blair <kblair40@gmail.com>
2022-10-26 15:27:33 -0500

    Add skeleton for settings form

---
+++ src/components/Forms/SettingsForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Stack,
+  Flex,
+  Text,
+  Box,
+  Input,
+  FormControl,
+  FormLabel,
+  Select,
+} from "@chakra-ui/react";
+
+const SettingsForm = ({ cardSide = "front" }) => {
+  return <Flex direction="column" align="center"></Flex>;
+};
+
+export default SettingsForm;
