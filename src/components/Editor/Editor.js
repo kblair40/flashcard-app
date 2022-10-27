19e31b0d53947f38b90abaefe2cedfb7dee1960d
Kevin Blair <kblair40@gmail.com>
2022-10-27 09:59:28 -0500

    Apply user styles to editor by default

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,30 +1,70 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useContext, useState } from "react";
 import { Flex, Box, useColorMode } from "@chakra-ui/react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "editor.css";
 
+import UserContext from "store/UserContext";
 import { quillSettings } from "utils/constants";
 
+const styleMap = {
+  bold: ["bold", "true"],
+  italic: ["italic", "true"],
+  underline: ["underline", "true"],
+  left: ["align", "left"],
+  center: ["align", "center"],
+  right: ["align", "right"],
+  justify: ["align", "justify"],
+  small: ["size", "small"],
+  medium: ["size", "normal"], // no such size as "medium" with quill
+  large: ["size", "large"],
+  huge: ["size", "huge"],
+};
+
 const Editor = ({ value, onChange, cardSide }) => {
+  const [defaultStyles, setDefaultStyles] = useState();
+
   const { colorMode } = useColorMode();
   const isDark = colorMode === "dark";
+
+  const { userData } = useContext(UserContext);
 
   const quill = useRef(null);
   // const didMount = useRef(false);
 
-  // useEffect(() => {
-  //   if (!didMount.current) {
-  //     if (cardSide === "front") {
-  //       let editor = quill.current.getEditor();
-  //       editor.format("bold", "true");
-  //       editor.format("align", "center");
-  //       editor.focus();
-  //     }
-  //   } else {
-  //     didMount.current = true;
-  //   }
-  // }, [cardSide]);
+  useEffect(() => {
+    const getDefaultStyles = (styles) => {
+      if (!styles) return;
+
+      let defaultStyles = [];
+
+      if (styles.isBold) defaultStyles.push(styleMap.bold);
+      if (styles.isItalic) defaultStyles.push(styleMap.italic);
+      if (styles.isUnderlined) defaultStyles.push(styleMap.underline);
+      if (styles.textAlign !== "left") {
+        defaultStyles.push(styleMap[styles["textAlign"]]);
+      }
+      if (styles.fontSize !== "medium") {
+        defaultStyles.push(styleMap[styles["fontSize"]]);
+      }
+
+      const editor = quill.current.getEditor();
+      for (let style of defaultStyles) {
+        editor.format(...style);
+      }
+
+      if (cardSide === "front") {
+        // only focus front side
+        editor.focus();
+      }
+    };
+
+    if (userData && userData.default_styles && quill.current) {
+      const styles = userData.default_styles[cardSide];
+      console.log("\nstyles:", styles);
+      getDefaultStyles(styles);
+    }
+  }, [userData, cardSide]);
 
   return (
     <Flex justify="center" w="100%">
