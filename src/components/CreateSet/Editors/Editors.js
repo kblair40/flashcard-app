92390d22cd7f0c4c81e983531dadd65fb31fd065
Kevin Blair <kblair40@gmail.com>
2022-10-27 10:38:50 -0500

    Move style setting logic/functionality to Editors component

--- src/components/CreateSet/Editors/Editors.js
+++ src/components/CreateSet/Editors/Editors.js
@@ -1,8 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import { Box, VStack } from "@chakra-ui/react";
 
+import UserContext from "store/UserContext";
 import SetContext from "store/SetContext";
 import Editor from "components/Editor";
+
+const styleMap = {
+  bold: ["bold", "true"],
+  italic: ["italic", "true"],
+  underline: ["underline", "true"],
+  left: ["align", "left"],
+  center: ["align", "center"],
+  right: ["align", "right"],
+  justify: ["align", "justify"],
+  small: ["size", "small"],
+  medium: ["size", "normal"], // no such size as "medium" with quill
+  large: ["size", "large"],
+  huge: ["size", "huge"],
+};
 
 const Editors = ({ width = "100%", height = "100%" }) => {
   const {
@@ -10,7 +25,55 @@ const Editors = ({ width = "100%", height = "100%" }) => {
     setFrontCardContent,
     backCardContent,
     setBackCardContent,
+    flashcardSetData,
   } = useContext(SetContext);
+
+  useEffect(() => {
+    console.log("SET DATA:", flashcardSetData);
+  }, [flashcardSetData]);
+
+  const { userData } = useContext(UserContext);
+
+  const frontRef = useRef();
+  const backRef = useRef();
+
+  useEffect(() => {
+    const getDefaultStyles = (styles, cardSide) => {
+      if (!styles) return;
+
+      let defaultStyles = [];
+
+      if (styles.isBold) defaultStyles.push(styleMap.bold);
+      if (styles.isItalic) defaultStyles.push(styleMap.italic);
+      if (styles.isUnderlined) defaultStyles.push(styleMap.underline);
+      if (styles.textAlign !== "left") {
+        defaultStyles.push(styleMap[styles["textAlign"]]);
+      }
+      if (styles.fontSize !== "medium") {
+        defaultStyles.push(styleMap[styles["fontSize"]]);
+      }
+
+      let ref = cardSide === "front" ? frontRef : backRef;
+      const editor = ref.current.getEditor();
+      for (let style of defaultStyles) {
+        editor.format(...style);
+      }
+
+      // regardless of back or front, make sure front gets focused first
+      frontRef.current.focus();
+    };
+
+    if (userData && userData.default_styles) {
+      if (frontRef.current) {
+        const styles = userData.default_styles.front;
+        getDefaultStyles(styles, "front");
+      }
+      if (backRef.current) {
+        const styles = userData.default_styles.back;
+        getDefaultStyles(styles, "back");
+      }
+    }
+  }, [userData, flashcardSetData]);
 
   return (
     <VStack
@@ -22,6 +85,7 @@ const Editors = ({ width = "100%", height = "100%" }) => {
     >
       <Box w="100%" maxWidth="700px">
         <Editor
+          ref={frontRef}
           cardSide="front"
           value={frontCardContent}
           onChange={setFrontCardContent}
@@ -30,6 +94,7 @@ const Editors = ({ width = "100%", height = "100%" }) => {
 
       <Box w="100%" maxW="700px" pb="1rem">
         <Editor
+          ref={backRef}
           cardSide="back"
           value={backCardContent}
           onChange={setBackCardContent}
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,70 +1,14 @@
-import React, { useRef, useEffect, useContext, useState } from "react";
+import React from "react";
 import { Flex, Box, useColorMode } from "@chakra-ui/react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "editor.css";
 
-import UserContext from "store/UserContext";
 import { quillSettings } from "utils/constants";
 
-const styleMap = {
-  bold: ["bold", "true"],
-  italic: ["italic", "true"],
-  underline: ["underline", "true"],
-  left: ["align", "left"],
-  center: ["align", "center"],
-  right: ["align", "right"],
-  justify: ["align", "justify"],
-  small: ["size", "small"],
-  medium: ["size", "normal"], // no such size as "medium" with quill
-  large: ["size", "large"],
-  huge: ["size", "huge"],
-};
-
-const Editor = ({ value, onChange, cardSide }) => {
-  const [defaultStyles, setDefaultStyles] = useState();
-
+const Editor = React.forwardRef(({ value, onChange }, ref) => {
   const { colorMode } = useColorMode();
   const isDark = colorMode === "dark";
-
-  const { userData } = useContext(UserContext);
-
-  const quill = useRef(null);
-  // const didMount = useRef(false);
-
-  useEffect(() => {
-    const getDefaultStyles = (styles) => {
-      if (!styles) return;
-
-      let defaultStyles = [];
-
-      if (styles.isBold) defaultStyles.push(styleMap.bold);
-      if (styles.isItalic) defaultStyles.push(styleMap.italic);
-      if (styles.isUnderlined) defaultStyles.push(styleMap.underline);
-      if (styles.textAlign !== "left") {
-        defaultStyles.push(styleMap[styles["textAlign"]]);
-      }
-      if (styles.fontSize !== "medium") {
-        defaultStyles.push(styleMap[styles["fontSize"]]);
-      }
-
-      const editor = quill.current.getEditor();
-      for (let style of defaultStyles) {
-        editor.format(...style);
-      }
-
-      if (cardSide === "front") {
-        // only focus front side
-        editor.focus();
-      }
-    };
-
-    if (userData && userData.default_styles && quill.current) {
-      const styles = userData.default_styles[cardSide];
-      console.log("\nstyles:", styles);
-      getDefaultStyles(styles);
-    }
-  }, [userData, cardSide]);
 
   return (
     <Flex justify="center" w="100%">
@@ -77,7 +21,7 @@ const Editor = ({ value, onChange, cardSide }) => {
         }}
       >
         <ReactQuill
-          ref={quill}
+          ref={ref}
           className={
             isDark
               ? "custom-editor-dark custom-editor"
@@ -88,18 +32,11 @@ const Editor = ({ value, onChange, cardSide }) => {
           value={value}
           onChange={onChange}
           preserveWhitespace={true}
-          onEditorCreated={() => console.log("\n\nCREATED\n\n")}
           // formats={quillSettings.formats}
         />
       </Box>
     </Flex>
   );
-};
+});
 
 export default Editor;
-
-// rich-markdown-editor - 337kb
-// draft-js - 65kb
-// jodit-react - 221kb
-// react-froala-wysiwyg - 125kb
-// react-quill - 51kb
