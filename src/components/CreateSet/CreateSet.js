f917daa31deb12681e1c2b9cbc5f5d09a760d7ff
Kevin Blair <kblair40@gmail.com>
2023-01-06 12:10:13 -0600

    Move SetMeta into separate file/component

--- src/components/CreateSet/CreateSet.js
+++ src/components/CreateSet/CreateSet.js
@@ -1,11 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Flex, Text, IconButton, Box, Grid, GridItem } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
-import { EditIcon } from "utils/icons";
 import SetContext from "store/SetContext";
 import Sidebar from "./Sidebar";
 import Editors from "./Editors";
-import EditModal from "components/Modals/EditModal";
+import SetMeta from "./SetMeta";
 
 const CreateSet = () => {
   const { flashcardSetData: setData } = useContext(SetContext);
@@ -53,112 +52,112 @@ const CreateSet = () => {
 
 export default CreateSet;
 
-const SetMeta = ({ setData, width = "100%" }) => {
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [localData, setLocalData] = useState();
+// const SetMeta = ({ setData, width = "100%" }) => {
+//   const [showEditModal, setShowEditModal] = useState(false);
+//   const [localData, setLocalData] = useState();
 
-  let category, title;
-  let data = {};
+//   let category, title;
+//   let data = {};
 
-  let border = "1px solid #ccc";
+//   let border = "1px solid #ccc";
 
-  useEffect(() => {
-    if (setData) setLocalData(setData);
-  }, [setData]);
+//   useEffect(() => {
+//     if (setData) setLocalData(setData);
+//   }, [setData]);
 
-  if (!localData) {
-    return <Box h="70px" border={border} />;
-  }
+//   if (!localData) {
+//     return <Box h="70px" border={border} />;
+//   }
 
-  if (localData) {
-    category = localData.category;
-    title = localData.title;
+//   if (localData) {
+//     category = localData.category;
+//     title = localData.title;
 
-    data = {
-      category: localData.category,
-      title: localData.title,
-      desc: localData.description,
-      id: localData._id,
-    };
-  }
+//     data = {
+//       category: localData.category,
+//       title: localData.title,
+//       desc: localData.description,
+//       id: localData._id,
+//     };
+//   }
 
-  return (
-    <Flex
-      h={{ base: "100%", sm: "70px" }}
-      align="center"
-      width={width}
-      pr={{ base: "8px", sm: "16px" }}
-      pl={{ base: "8px", sm: "0" }}
-      flexWrap="wrap"
-      justifyContent="center"
-    >
-      <Flex h={{ base: "max-content" }} w="100%" maxWidth="700px">
-        <Flex
-          w={{ base: "35%" }}
-          direction="row"
-          flexWrap="wrap"
-          align="center"
-          mr="1.25rem"
-        >
-          <Text fontWeight={600} mr="6px" noOfLines={1}>
-            Category:
-          </Text>
-          <Text
-            fontWeight={500}
-            fontSize="sm"
-            textTransform="capitalize"
-            noOfLines={1}
-          >
-            {category}
-          </Text>
-        </Flex>
+//   return (
+//     <Flex
+//       h={{ base: "100%", sm: "70px" }}
+//       align="center"
+//       width={width}
+//       pr={{ base: "8px", sm: "16px" }}
+//       pl={{ base: "8px", sm: "0" }}
+//       flexWrap="wrap"
+//       justifyContent="center"
+//     >
+//       <Flex h={{ base: "max-content" }} w="100%" maxWidth="700px">
+//         <Flex
+//           w={{ base: "35%" }}
+//           direction="row"
+//           flexWrap="wrap"
+//           align="center"
+//           mr="1.25rem"
+//         >
+//           <Text fontWeight={600} mr="6px" noOfLines={1}>
+//             Category:
+//           </Text>
+//           <Text
+//             fontWeight={500}
+//             fontSize="sm"
+//             textTransform="capitalize"
+//             noOfLines={1}
+//           >
+//             {category}
+//           </Text>
+//         </Flex>
 
-        <Flex
-          w={{ base: "35%" }}
-          direction="row"
-          flexWrap="wrap"
-          align="center"
-        >
-          <Text fontWeight={600} mr="6px" noOfLines={1}>
-            Title:
-          </Text>
-          {title && (
-            <Text
-              fontWeight={500}
-              fontSize="sm"
-              textTransform="capitalize"
-              noOfLines={1}
-            >
-              {title}
-            </Text>
-          )}
-        </Flex>
+//         <Flex
+//           w={{ base: "35%" }}
+//           direction="row"
+//           flexWrap="wrap"
+//           align="center"
+//         >
+//           <Text fontWeight={600} mr="6px" noOfLines={1}>
+//             Title:
+//           </Text>
+//           {title && (
+//             <Text
+//               fontWeight={500}
+//               fontSize="sm"
+//               textTransform="capitalize"
+//               noOfLines={1}
+//             >
+//               {title}
+//             </Text>
+//           )}
+//         </Flex>
 
-        <Flex
-          w={{ base: "30%" }}
-          direction="row"
-          flexWrap="wrap"
-          align="center"
-          justify="end"
-        >
-          <IconButton
-            onClick={() => setShowEditModal((prev) => !prev)}
-            variant="icon-button"
-            icon={<EditIcon boxSize="18px" />}
-          />
-        </Flex>
-      </Flex>
+//         <Flex
+//           w={{ base: "30%" }}
+//           direction="row"
+//           flexWrap="wrap"
+//           align="center"
+//           justify="end"
+//         >
+//           <IconButton
+//             onClick={() => setShowEditModal((prev) => !prev)}
+//             variant="icon-button"
+//             icon={<EditIcon boxSize="18px" />}
+//           />
+//         </Flex>
+//       </Flex>
 
-      {showEditModal && (
-        <EditModal
-          setData={data}
-          onPatchSuccess={({ category, title }) => {
-            setLocalData({ ...localData, category, title });
-          }}
-          isOpen={showEditModal}
-          onClose={() => setShowEditModal(false)}
-        />
-      )}
-    </Flex>
-  );
-};
+//       {showEditModal && (
+//         <EditModal
+//           setData={data}
+//           onPatchSuccess={({ category, title }) => {
+//             setLocalData({ ...localData, category, title });
+//           }}
+//           isOpen={showEditModal}
+//           onClose={() => setShowEditModal(false)}
+//         />
+//       )}
+//     </Flex>
+//   );
+// };
