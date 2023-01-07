3548ec95afd5637d6cc9ed2930b910c6e7762fc7
Kevin Blair <kblair40@gmail.com>
2023-01-07 10:49:12 -0600

    Fix axios error from es

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<App />, div);
+// });
+
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+it("renders welcome message", () => {
+  render(<App />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
 });
